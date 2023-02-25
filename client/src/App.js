c0933daefdd9725867016b78705a22399dd5bf4e
007AxelBerkati <bucel1916@gmail.com>
2023-02-25 13:17:58 +0700

    JSX Structures

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,38 @@
 import React from 'react'
+import {AppBar, Container, Grid, Grow, Typography} from  '@material-ui/core'
+import memories from './images/memories.png'
+import Posts from './components/Posts/Posts'
+import Form from './components/Form/Form'
 
 const App = () => {
   return (
-    <div>App</div>
+    <Container maxWidth={'lg'}>
+      <AppBar position='static' color='inherit'>
+        <Typography variant='h2' align='center'>
+          Memories
+        </Typography>
+        <img 
+          src={memories}
+          alt='memories'
+          height='60'
+        />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justifyContent='space-between' alignItems='stretch' spacing={4}>
+            <Grid item xs={12} sm={7}>
+              <Posts />
+
+              </Grid>
+            <Grid item xs={12} sm={7}>
+              <Form />
+
+              </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+
+    </Container>
   )
 }
 
