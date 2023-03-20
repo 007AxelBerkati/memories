2207f685a3c7d243aba12b388a1cf38af1a51f09
007AxelBerkati <bucel1916@gmail.com>
2023-03-20 15:51:07 +0700

    add routes for get Post by search and get Post

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -7,15 +7,18 @@ import {
   updatePost,
   likePost,
   deletePost,
+  getPostsBySearch,
 } from '../controllers/posts.js';
 
 import auth from '../middleware/auth.js';
 
 const router = express.Router();
 
+router.get('/search', getPostsBySearch);
 router.get('/', getPosts);
+router.get('/:id', getPost);
+
 router.post('/', auth, createPost);
-// router.get('/:id', getPost);
 router.patch('/:id', auth, updatePost);
 router.delete('/:id', deletePost);
 router.patch('/:id/likePost', auth, likePost);
