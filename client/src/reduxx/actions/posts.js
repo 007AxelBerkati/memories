b4e03148239bd1a0d2e8142a6586145a2714df24
007AxelBerkati <bucel1916@gmail.com>
2023-03-20 15:49:51 +0700

    update : redux action and reducers

--- client/src/reduxx/actions/posts.js
+++ client/src/reduxx/actions/posts.js
@@ -1,59 +1,101 @@
 import * as api from '../../api';
-import { CREATE, DELETE, FETCH_ALL, LIKE, LOADING, UPDATE } from '../types';
+import {
+  CREATE,
+  DELETE,
+  END_LOADING,
+  FETCH_ALL,
+  FETCH_BY_SEARCH,
+  FETCH_POST,
+  LIKE,
+  LOADING,
+  START_LOADING,
+  UPDATE,
+} from '../types';
 
 // Action Creators
-export const getPosts = () => async (dispatch) => {
-  dispatch({ type: LOADING, loading: true });
+export const getPost = (id) => async (dispatch) => {
   try {
-    const { data } = await api.fetchPosts();
+    dispatch({ type: START_LOADING });
 
-    dispatch({ type: FETCH_ALL, payload: data });
-    dispatch({ type: LOADING, loading: false });
+    const { data } = await api.fetchPost(id);
+
+    dispatch({ type: FETCH_POST, payload: { post: data } });
   } catch (error) {
-    console.log(error.message);
-    dispatch({ type: LOADING, loading: false });
+    console.log(error);
   }
 };
 
-export const createPost = (post) => async (dispatch) => {
+export const getPosts = (page) => async (dispatch) => {
   try {
+    dispatch({ type: START_LOADING });
+    const {
+      data: { data, currentPage, numberOfPages },
+    } = await api.fetchPosts(page);
+
+    dispatch({
+      type: FETCH_ALL,
+      payload: { data, currentPage, numberOfPages },
+    });
+    dispatch({ type: END_LOADING });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getPostsBySearch = (searchQuery) => async (dispatch) => {
+  try {
+    dispatch({ type: START_LOADING });
+    const {
+      data: { data },
+    } = await api.fetchPostsBySearch(searchQuery);
+
+    dispatch({ type: FETCH_BY_SEARCH, payload: { data } });
+    dispatch({ type: END_LOADING });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const createPost = (post, history) => async (dispatch) => {
+  try {
+    dispatch({ type: START_LOADING });
     const { data } = await api.createPost(post);
 
     dispatch({ type: CREATE, payload: data });
+
+    history.push(`/posts/${data._id}`);
   } catch (error) {
     console.log(error);
   }
 };
 
 export const updatePost = (id, post) => async (dispatch) => {
-  dispatch({ type: LOADING, loading: true });
-
   try {
     const { data } = await api.updatePost(id, post);
 
     dispatch({ type: UPDATE, payload: data });
-    dispatch({ type: LOADING, loading: true });
-  } catch (error) {
-    console.log(error);
-    dispatch({ type: LOADING, loading: true });
-  }
-};
-
-export const deletePost = (id) => async (dispatch) => {
-  try {
-    await api.deletePost(id);
-
-    dispatch({ type: DELETE, payload: id });
   } catch (error) {
     console.log(error);
   }
 };
 
 export const likePost = (id) => async (dispatch) => {
+  const user = JSON.parse(localStorage.getItem('profile'));
+
   try {
-    const { data } = await api.likePost(id);
+    const { data } = await api.likePost(id, user?.token);
 
     dispatch({ type: LIKE, payload: data });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const deletePost = (id) => async (dispatch) => {
+  try {
+    await await api.deletePost(id);
+
+    dispatch({ type: DELETE, payload: id });
   } catch (error) {
     console.log(error);
   }
--- client/src/reduxx/reducers/posts.js
+++ client/src/reduxx/reducers/posts.js
@@ -1,37 +1,37 @@
-import { CREATE, DELETE, FETCH_ALL, LIKE, UPDATE } from '../types';
+import {
+  CREATE,
+  DELETE,
+  END_LOADING,
+  FETCH_ALL,
+  FETCH_BY_SEARCH,
+  FETCH_POST,
+  LIKE,
+  START_LOADING,
+  UPDATE,
+} from '../types';
 
 const initialStatePost = {
   posts: [],
-  loading: false,
+  loading: true,
 };
 
 export const postReducer = (state = initialStatePost, action) => {
   switch (action.type) {
+    case START_LOADING:
+      return { ...state, isLoading: true };
+    case END_LOADING:
+      return { ...state, isLoading: false };
     case FETCH_ALL:
       return {
         ...state,
-        posts: action.payload,
+        posts: action.payload.data,
+        currentPage: action.payload.currentPage,
+        numberOfPages: action.payload.numberOfPages,
       };
-    case CREATE:
-      return {
-        ...state,
-        posts: [...state.posts, action.payload],
-      };
-
-    case UPDATE:
-      return {
-        ...state,
-        posts: state.posts.map((post) =>
-          post._id === action.payload._id ? action.payload : post
-        ),
-      };
-
-    case DELETE:
-      return {
-        ...state,
-        posts: state.posts.filter((post) => post._id !== action.payload),
-      };
-
+    case FETCH_BY_SEARCH:
+      return { ...state, posts: action.payload.data };
+    case FETCH_POST:
+      return { ...state, post: action.payload.post };
     case LIKE:
       return {
         ...state,
@@ -39,7 +39,20 @@ export const postReducer = (state = initialStatePost, action) => {
           post._id === action.payload._id ? action.payload : post
         ),
       };
-
+    case CREATE:
+      return { ...state, posts: [...state.posts, action.payload] };
+    case UPDATE:
+      return {
+        ...state,
+        posts: state.posts.map((post) =>
+          post._id === action.payload._id ? action.payload : post
+        ),
+      };
+    case DELETE:
+      return {
+        ...state,
+        posts: state.posts.filter((post) => post._id !== action.payload),
+      };
     default:
       return state;
   }
