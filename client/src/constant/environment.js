539baf075048b10bfb6550464ab7bc900ceab21c
007AxelBerkati <bucel1916@gmail.com>
2023-03-22 13:25:07 +0700

    update : env google ID

--- client/src/constant/environment.js
+++ client/src/constant/environment.js
@@ -3,4 +3,7 @@ export const backendAPI =
     ? process.env.REACT_APP_API_URL
     : 'http://localhost:5000';
 
-export const googleClientId = process.env.REACT_APP_OAUTH_CLIENT_ID;
+export const googleClientId =
+  process.env.NODE_ENV === 'production'
+    ? process.env.REACT_APP_OAUTH_CLIENT_ID
+    : process.env.REACT_APP_OAUTH_CLIENT_ID;
