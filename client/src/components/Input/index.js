ce7969aa3671b5030f0535e321af70be38c7d7c1
007AxelBerkati <bucel1916@gmail.com>
2023-04-08 16:41:40 +0700

    styles : input component

--- client/src/components/Input/index.js
+++ client/src/components/Input/index.js
@@ -3,6 +3,7 @@ import { TextField, Grid, InputAdornment, IconButton } from '@material-ui/core';
 
 import Visibility from '@material-ui/icons/Visibility';
 import VisibilityOff from '@material-ui/icons/VisibilityOff';
+import useStyles from './styles';
 
 const Input = ({
   half,
@@ -13,12 +14,15 @@ const Input = ({
   type,
   handleShowPassword,
 }) => {
+  const classes = useStyles();
+
   return (
     <Grid item xs={12} sm={half ? 6 : 12}>
       <TextField
         name={name}
         label={label}
         onChange={handleChange}
+        className={classes.input}
         variant="outlined"
         required
         fullWidth
@@ -30,13 +34,18 @@ const Input = ({
                 endAdornment: (
                   <InputAdornment position="end">
                     <IconButton onClick={handleShowPassword}>
-                      {type === 'password' ? <Visibility /> : <VisibilityOff />}
+                      {type === 'password' ? (
+                        <Visibility style={{ color: '#2A86FF' }} />
+                      ) : (
+                        <VisibilityOff style={{ color: '#2A86FF' }} />
+                      )}
                     </IconButton>
                   </InputAdornment>
                 ),
               }
             : null
         }
+        inputProps={{ style: { color: 'white' } }}
       />
     </Grid>
   );
